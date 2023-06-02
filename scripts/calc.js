427aa506d6adec793382b5966414c90f87796a22
siobhain <>
2023-06-02 18:04:20 +0000

    expand test to include undefined

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,5 +1,9 @@
-function addition() {
-    return 42;
+function addition(num1, num2) {
+    if (Number.isFinite(num1) && Number.isFinite(num2)) {
+        return num1 + num2;
+    } else {
+        return TypeError;
+    }
 }
 
 module.exports = addition;
--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -5,6 +5,12 @@ describe("Calculator", () => {
         test("should return 42 for 20 + 22", () => {
             expect(addition(20, 22)).toBe(42);
         });
+        test("should return 55 for 33 + 22", () => {
+            expect(addition(33, 22)).toBe(55);
+        });
+        test("should return invalid input for 33 + donkey", () => {
+            expect(addition(33, donkey)).toBeUndefined();
+        });
     });
     describe("Subtract function", () => {
 
